2dfa03e973b33fe62b2d37df4dd94f8a5e13a51d
h4ckm1n <97511408+h4ckm1n-dev@users.noreply.github.com>
2025-03-10 14:05:49 +0700

    caching website

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'menu-cache-v1';
+const urlsToCache = [
+    'https://docs.google.com/gview?url=https://drive.google.com/uc?id=1KY8vSR1zdysLWHWT_MjiaKoMUCvN9qKm&embedded=true',
+    'https://docs.google.com/gview?url=https://drive.google.com/uc?id=1oVyHAZ5xnMceZcolkbiZC6U99qNIzct3&embedded=true',
+    'https://docs.google.com/gview?url=https://drive.google.com/uc?id=1aIc6KCSoPyrcZIe4Z_6b9MSSh-N7neyy&embedded=true',
+    'https://docs.google.com/gview?url=https://drive.google.com/uc?id=140piNM28iJzAVrxEKVpQrOxdCsaHf6kB&embedded=true'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+}); 
